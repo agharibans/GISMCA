8ae565af00824923e4f46502e67b292a4239c09c
Armen Gharibans <5421937+agharibans@users.noreply.github.com>
2020-01-29 11:40:46 +1300

    Update setup.py
    bug fix

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ setup(
 	author = 'Armen Gharibans',
 	author_email = 'armen@alimetry.com',
     python_requires = "~=3.6.9",
-    install_requires = ["numpy~=1.17.5","scipy~=1.4.1","matplotlib~=3.1.2","pandas~=0.25.3,scikit-image~=0.16.2"],
+    install_requires = ["numpy~=1.17.5","scipy~=1.4.1","matplotlib~=3.1.2","pandas~=0.25.3","scikit-image~=0.16.2"],
 	packages = ['GISMCA'],
 	license = 'MIT',
 	zip_safe = False
