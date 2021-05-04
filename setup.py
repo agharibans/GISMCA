779cf801c323b86e54e0784bcd3eef38882c26fd
Armen Gharibans <5421937+agharibans@users.noreply.github.com>
2021-05-05 06:56:51 +1200

    updated to Python 3.7

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ setup(
 	url = 'https://github.com/agharibans/GISMCA.git',
 	author = 'Armen Gharibans',
 	author_email = 'armen@alimetry.com',
-    python_requires = "~=3.6.9",
+    python_requires = "~=3.7",
     install_requires = ["numpy~=1.17","scipy~=1.4","matplotlib~=3.1","pandas~=1.0","scikit-image~=0.16"],
 	packages = ['GISMCA'],
 	license = 'MIT',
